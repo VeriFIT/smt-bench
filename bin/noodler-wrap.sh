40d7184f0a8328bcde319d58cd1f39dcd60cbb41
Ondra Lengal <ondra.lengal@gmail.com>
2022-08-05 12:37:55 +0200

    noodler wrapper

--- bin/noodler-wrap.sh
+++ bin/noodler-wrap.sh
@@ -10,10 +10,19 @@ INPUT=$1
 shift
 PARAMS="$*"
 
-NOODLER="../bin/Noodler/noodler.py"
+SCRIPT_PATH=$(dirname $0)
+NOODLER_DIR="${SCRIPT_PATH}/Noodler"
+NOODLER_EXE="noodler.py"
 
-out=$(${NOODLER} ${INPUT} $PARAMS | grep -v "^\[Warning\]")
+(
+  cd ${NOODLER_DIR}
+  version=$(./${NOODLER_EXE} --version | grep -v "^\[Warning\]")
+  echo "version: ${version}"
+)
+
+#out=$(${NOODLER_DIR}/${NOODLER_EXE} ${INPUT} $PARAMS | grep -v "^\[Warning\]")
+${NOODLER_DIR}/${NOODLER_EXE} ${INPUT} --csv $PARAMS | grep -v "^\[Warning\]"
 ret=$?
-echo "result: ${out}"
+#echo "result: ${out}"
 
 exit ${ret}
