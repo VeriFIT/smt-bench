bbb2a114fc2755244c6fc1ae1f9f4d1b2a84206a
jurajsic <juraj.sic@outlook.com>
2023-02-15 10:17:45 +0100

    z3-noodler wrapper update

--- bin/z3-noodler-wrap.sh
+++ bin/z3-noodler-wrap.sh
@@ -8,6 +8,9 @@ fi
 
 INPUT=$1
 
+ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
+SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
+
 out=$("${SCRIPT_DIR}/z3-noodler/build/z3" smt.string_solver="noodler" -smt2 ${INPUT})
 ret=$?
 echo "result: ${out}"
