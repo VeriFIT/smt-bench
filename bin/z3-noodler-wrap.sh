3563416bdc39e44fa2144d8da5430453f64ad772
jurajsic <juraj.sic@outlook.com>
2023-02-15 10:02:48 +0100

    added wrapper for z3-noodler

---
+++ bin/z3-noodler-wrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check the number of command-line arguments
+if [ \( "$#" -ne 1 \) ] ; then
+	echo "usage: ${0} <input>"
+	exit 1
+fi
+
+INPUT=$1
+
+out=$(./z3-noodler/build/z3 smt.string_solver="noodler" -smt2 ${INPUT})
+ret=$?
+echo "result: ${out}"
+
+exit ${ret}
