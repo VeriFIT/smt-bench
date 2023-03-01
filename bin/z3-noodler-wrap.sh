981502802fb3f51eb5f8dcdaaa82c99472168c97
jurajsic <juraj.sic@outlook.com>
2023-03-01 16:59:49 +0100

    add z3-noodler version to output

--- bin/z3-noodler-wrap.sh
+++ bin/z3-noodler-wrap.sh
@@ -11,8 +11,12 @@ INPUT=$1
 ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
 SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
 
-out=$("${SCRIPT_DIR}/z3-noodler/build/z3" smt.string_solver="noodler" -smt2 ${INPUT})
+z3_noodler_exe="${SCRIPT_DIR}/z3-noodler/build/z3"
+z3_noodler_version_string=($("$z3_noodler_exe" --version))
+z3_noodler_git_hash=${z3_noodler_version_string[9]}
+
+out=$("$z3_noodler_exe" smt.string_solver="noodler" -smt2 ${INPUT})
 ret=$?
-echo "result: ${out}"
+echo "$z3_noodler_git_hash-result: ${out}"
 
 exit ${ret}
