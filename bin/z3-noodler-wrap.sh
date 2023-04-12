dd3cc40ed1500fe131fdc119a58d342ec1b47b44
Juraj Síč <juraj.sic@outlook.com>
2023-04-12 12:48:48 +0200

    Update z3-noodler-wrap.sh

--- bin/z3-noodler-wrap.sh
+++ bin/z3-noodler-wrap.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Check the number of command-line arguments
-if [ \( "$#" -ne 1 \) ] ; then
-	echo "usage: ${0} <input>"
-	exit 1
-fi
-
-INPUT=$1
+INPUT=$*
 
 ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
 SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
