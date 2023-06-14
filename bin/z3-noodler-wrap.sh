1b8766e49d99bc475e4280f6e67c08d656174733
jurajsic <juraj.sic@outlook.com>
2023-06-14 19:10:55 +0200

    get mata git hash during z3-noodler run

--- bin/z3-noodler-wrap.sh
+++ bin/z3-noodler-wrap.sh
@@ -8,9 +8,10 @@ SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
 z3_noodler_exe="${SCRIPT_DIR}/z3-noodler/build/z3"
 z3_noodler_version_string=($("$z3_noodler_exe" --version))
 z3_noodler_git_hash=${z3_noodler_version_string[9]}
+mata_git_hash=${z3_noodler_version_string[13]}
 
 out=$("$z3_noodler_exe" smt.string_solver="noodler" -smt2 ${INPUT})
 ret=$?
-echo "${z3_noodler_git_hash:0:7}-result: ${out}"
+echo "${z3_noodler_git_hash:0:7}-${mata_git_hash:0:7}-result: ${out}"
 
 exit ${ret}
