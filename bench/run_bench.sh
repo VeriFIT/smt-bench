303e29db1a95f72a12600110670a22ad5ace05b3
jurajsic <juraj.sic@outlook.com>
2023-08-23 17:08:57 +0200

    run all benchmarks by default

--- bench/run_bench.sh
+++ bench/run_bench.sh
@@ -2,21 +2,26 @@
 
 show_help() {
 	echo "Usage:"
-	echo "run_bench.sh [options] BENCHMARK1 [BENCHMARK2 BENCHMARK3 ...]"
+	echo "run_bench.sh [options] [BENCHMARK1 BENCHMARK2 BENCHMARK3 ...]"
 	echo ""
-	echo "Runs TOOL on given benchmarkd. If BENCHMARKi can have one"
+	echo "Runs given tool on given benchmarks. BENCHMARKi can have one"
 	echo "of the special values 'quick'/'slow'/'all', the script then"
 	echo "runs selection of quick/slow benchmarks or all benchmarks"
 	echo "respectively. Note that if benchmark is given twice (for"
-	echo "example it is in quick, but also given explicitly), then"
-	echo "it will be run twice."
-  echo "Options:"
-  echo "  -h     Show this help message"
-  echi "  -t     Which tool to run (default=z3-noodler)"
-  echo "  -j N   How many processes to run in parallel (default=8)"
+	echo "example it is in quick, but also given explicitly), then it"
+	echo "will be run twice. If no benchmark is given, all benchmarks"
+	echo "are run."
+	echo "Options:"
+	echo "  -h     Show this help message"
+	echi "  -t     Which tool to run (default=z3-noodler)"
+	echo "  -j N   How many processes to run in parallel (default=8)"
 }
 
-TOOL="z3-noodler"
+QUICK=("sygus_qgen" "norn" "slog" "slent" "denghang" "leetcode")
+SLOW=("automatark" "str_small_rw" "full_str_int" "transducer_plus" "kaluza" "stringfuzz" "woorpje" "webapp" "kepler" "pyex")
+ALL=$QUICK+$SLOW
+
+tool="z3-noodler"
 j_value="8"
 while getopts "hj:" option; do
     case $option in
@@ -25,7 +30,7 @@ while getopts "hj:" option; do
             exit 0
             ;;
         t)
-            TOOL=$OPTARG
+            tool=$OPTARG
             ;;
         j)
             j_value=$OPTARG
@@ -41,25 +46,21 @@ done
 # Shift the option index so that $1 refers to the first positional argument
 shift $((OPTIND - 1))
 
+benchmarks=()
+
+# If no benchmark is given, run all
 if [ -z "$1" ]; then
-  echo "Expected name of benchmark"
-  show_help
-  exit 1
+  benchmarks=("${ALL[@]}")
 fi
 
-quick=("sygus_qgen" "norn" "slog" "slent" "denghang" "leetcode")
-slow=("automatark" "str_small_rw" "full_str_int" "transducer_plus" "kaluza" "stringfuzz" "woorpje" "webapp" "kepler" "pyex")
-
-benchmarks=()
 for BENCH_NAME in "$@"
 do
 	if [[ "$BENCH_NAME" == "quick" ]]; then
-		benchmarks+=("${quick[@]}")
+		benchmarks+=("${QUICK[@]}")
 	elif [[ "$BENCH_NAME" == "slow" ]]; then
-		benchmarks+=("${slow[@]}")
+		benchmarks+=("${SLOW[@]}")
 	elif [[ "$BENCH_NAME" == "all" ]]; then
-		benchmarks+=("${quick[@]}")
-		benchmarks+=("${slow[@]}")
+		benchmarks+=("${ALL[@]}")
 	else
 		benchmarks+=("$BENCH_NAME")
 	fi
@@ -69,8 +70,8 @@ tasks_files=()
 for benchmark in "${benchmarks[@]}"; do
 	echo "Running benchmark $benchmark"
 	CUR_DATE=$(date +%Y-%m-%d-%H-%M)
-	TASKS_FILE="$benchmark-to120-$TOOL-$CUR_DATE.tasks"
-	cat "$benchmark.input" | ./pycobench -c smt.yaml -j $j_value -t 120 -m "$TOOL" -o "$TASKS_FILE"
+	TASKS_FILE="$benchmark-to120-$tool-$CUR_DATE.tasks"
+	cat "$benchmark.input" | ./pycobench -c smt.yaml -j $j_value -t 120 -m "$tool" -o "$TASKS_FILE"
 	tasks_files+=("$TASKS_FILE")
 	echo "$TASKS_FILE" >> tasks_names.txt
 done
