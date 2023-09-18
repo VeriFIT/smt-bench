21ee74449b066d2c31aba006dcd1345f802d62a0
jurajsic <juraj.sic@outlook.com>
2023-09-18 09:00:54 +0200

    updated z3-trau wrapper

--- bin/z3-trau-wrap.sh
+++ bin/z3-trau-wrap.sh
@@ -12,10 +12,12 @@ ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
 SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
 
 PROGRAM="${SCRIPT_DIR}/z3-trau/build/z3"
+# The last version of trau is 1.1, we assume we only run this
+VERSION="1.1"
 
 # we need to replace RegLan sort with (RegEx String) sort, re.none with re.empty, in_re and to_re with in.re and to.re, QF_SLIA logic with ALL logic, and \u{.}, \u{..} with #x0. and #x..
-out=$(cat ${INPUT} | sed 's|RegLan|(RegEx String)|g' | sed 's|re.none|re.empty|g' | sed 's|in_re|in.re|g' | sed 's|to_re|to.re|g' | sed 's|QF_SLIA|ALL|g' | sed -r 's|\\u\{(.)\}|#x0\1|g' | sed -r 's|\\u\{(..)\}|#x\1|g' | ${PROGRAM} smt.string_solver=trau -in)
+out=$(cat ${INPUT} | sed 's|RegLan|(RegEx String)|g' | sed 's|re.none|re.empty|g' | sed 's|in_re|in.re|g' | sed 's|to_re|to.re|g' | sed 's|QF_SLIA|ALL|g' | sed 's|str.to_int|str.to.int|g' | sed 's|str.from_int|int.to.str|g' | sed -r 's|\\u\{(.)\}|#x0\1|g' | sed -r 's|\\u\{(..)\}|#x\1|g' | ${PROGRAM} smt.string_solver=trau -in)
 ret=$?
-echo "result: ${out}"
+echo "$VERSION-result: ${out}"
 
 exit ${ret}
