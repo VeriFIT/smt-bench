b5294510d396a1c5a00b418ed6dad52c6df11c58
jurajsic <juraj.sic@outlook.com>
2023-09-19 15:19:30 +0200

    z3-trau wrapper

--- bin/z3-trau-wrap.sh
+++ bin/z3-trau-wrap.sh
@@ -16,7 +16,7 @@ PROGRAM="${SCRIPT_DIR}/z3-trau/build/z3"
 VERSION="1.1"
 
 # we need to replace RegLan sort with (RegEx String) sort, re.none with re.empty, in_re and to_re with in.re and to.re, QF_SLIA logic with ALL logic, and \u{.}, \u{..} with #x0. and #x..
-out=$(cat ${INPUT} | sed 's|RegLan|(RegEx String)|g' | sed 's|re.none|re.empty|g' | sed 's|in_re|in.re|g' | sed 's|to_re|to.re|g' | sed 's|QF_SLIA|ALL|g' | sed 's|str.to_int|str.to.int|g' | sed 's|str.from_int|int.to.str|g' | sed 's|re.comp|re.complement|g' | sed -r 's|\\u\{(.)\}|#x0\1|g' | sed -r 's|\\u\{(..)\}|#x\1|g' | ${PROGRAM} smt.string_solver=trau -in)
+out=$(cat ${INPUT} | sed 's|RegLan|(RegEx String)|g' | sed 's|re.none|re.empty|g' | sed 's|in_re|in.re|g' | sed 's|to_re|to.re|g' | sed 's|QF_SLIA|ALL|g' | sed 's|str.to_int|str.to.int|g' | sed 's|str.from_int|int.to.str|g' | sed 's|re.comp|re.complement|g' | sed -r 's|re\.\^ ([0-9]+)|re.loop \1 \1|g' | sed -r 's|\\u\{(.)\}|#x0\1|g' | sed -r 's|\\u\{(..)\}|#x\1|g' | ${PROGRAM} smt.string_solver=trau -in)
 ret=$?
 echo "$VERSION-result: ${out}"
 
