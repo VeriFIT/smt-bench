ef9bf43ab0c06e0af3c12ba95b7458d06ba27748
jurajsic <juraj.sic@outlook.com>
2024-07-16 14:56:56 +0200

    small fix 4

--- bin/check-models.sh
+++ bin/check-models.sh
@@ -25,7 +25,7 @@ if [ "$RESULT_OF_MODEL" = "sat" ]; then
   # replace stuff in model so that we have (assert (= var "its model"))
   add_to_input=$(echo "$MODEL" | sed 's/  (define-fun/(assert (=/g' | sed 's/ () String//g')
   add_to_input=$("$string_var"$'\n')
-  input_with_model="$(sed 's/(check_sat)//g; s/(exit)//g' "$INPUT")${add_to_input}(check-sat)"
+  input_with_model="$(sed 's/(check-sat)//g; s/(exit)//g' "$INPUT")${add_to_input}(check-sat)"
   out=$(echo "$input_with_model" | ${CVC_PROG} --lang smt2 $PARAMS)
   ret=$?
   echo "result: ${out}"
