e76a175b0b4a8e1e6203dea729bf0454d8e71fb2
jurajsic <juraj.sic@outlook.com>
2024-07-17 21:53:36 +0200

    enable model generation

--- bin/z3-noodler-model-wrap.sh
+++ bin/z3-noodler-model-wrap.sh
@@ -12,7 +12,7 @@ mata_git_hash=${z3_noodler_version_string[13]}
 
 out=$(sed '$i\
 (get-model)\
-' ${INPUT} | "$z3_noodler_exe" smt.string_solver="noodler" smt.str.produce_models=true -smt2 -in)
+' ${INPUT} | "$z3_noodler_exe" smt.string_solver="noodler" model=true -smt2 -in)
 ret=$?
 first_line=$(echo "$out" | head -n 1)
 echo "${z3_noodler_git_hash:0:7}-${mata_git_hash:0:7}-result: ${first_line}"
