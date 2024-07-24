fb1ab7a9e312e6097b42c9babe41d69fdfb131ff
jurajsic <juraj.sic@outlook.com>
2024-07-25 01:21:21 +0200

    add definition for cvc5 with model generation

---
+++ bin/cvc5-model-wrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check the number of command-line arguments
+if [ \( "$#" -lt 1 \) ] ; then
+	echo "usage: ${0} <input> [params]"
+	exit 1
+fi
+
+INPUT=$1
+shift
+PARAMS="$*"
+
+ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
+SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
+
+CVC_PROG="${SCRIPT_DIR}/cvc5-Linux-static/bin/cvc5"
+
+VERSION=$(${CVC_PROG} --version)
+VERSION=${VERSION#This is cvc5 version }
+VERSION=${VERSION% [*}
+
+out=$($(sed '$i\
+(get-model)\
+' ${INPUT} | ${CVC_PROG} --lang smt2 --produce-models $PARAMS)
+ret=$?
+first_line=$(echo "$out" | head -n 1)
+echo "$VERSION-result: ${first_line}"
+output_file="model-output-cvc5-${VERSION}/${INPUT:3}"
+output_dir=$(dirname $output_file)
+mkdir -p $output_dir
+echo "$out" > $output_file
+
+exit ${ret}
