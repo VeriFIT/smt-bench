519b9da079dae05983032105b299d8be2a7c5e9f
vhavlena <vohavlena@gmail.com>
2025-03-13 09:26:25 +0100

    wrappers: pldi'25 noodler notcont

---
+++ bin/z3-noodler-pldi-wrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INPUT=$*
+
+ABSOLUTE_SCRIPT_PATH=$(readlink -f $0)
+SCRIPT_DIR=$(dirname ${ABSOLUTE_SCRIPT_PATH})
+
+z3_noodler_exe="${SCRIPT_DIR}/z3-noodler/notcont-build/z3"
+z3_noodler_version_string=($("$z3_noodler_exe" --version))
+z3_noodler_git_hash=${z3_noodler_version_string[9]}
+mata_git_hash=${z3_noodler_version_string[13]}
+
+out=$("$z3_noodler_exe" smt.string_solver="noodler" -smt2 ${INPUT})
+ret=$?
+echo "${z3_noodler_git_hash:0:7}-${mata_git_hash:0:7}-result: ${out}"
+
+exit ${ret}
