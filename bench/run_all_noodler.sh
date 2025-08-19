f25c96798910513cc3705a3bd3335b08069fa06a
jurajsic <juraj.sic@outlook.com>
2025-08-19 22:54:41 +0200

    add snia for check-model in run_all_noodler.sh

--- bench/run_all_noodler.sh
+++ bench/run_all_noodler.sh
@@ -1,3 +1,3 @@
 unbuffer ./run_bench.sh all 2>&1 | tee --append run.log
 unbuffer ./run_bench.sh -t z3-noodler-model all 2>&1 | tee --append run.log
-unbuffer ./run_bench.sh -t check-model sygus_qgen denghang automatark stringfuzz hornstr norn slog slent omark kepler woorpje webapp kaluza transducer_plus leetcode str_small_rw pcp rna negated_predicates pyex full_str_int matching redos 2>&1 | tee --append run.log
+unbuffer ./run_bench.sh -t check-model sygus_qgen denghang automatark stringfuzz hornstr norn slog slent omark kepler woorpje webapp kaluza transducer_plus leetcode str_small_rw pcp rna negated_predicates pyex full_str_int snia matching redos 2>&1 | tee --append run.log
